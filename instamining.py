40ddc23a7f66517b0b721ff7cb9b51e666fc5b84
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-12-09 08:58:49 +0900

    Make insta tage click function and switch to each tap

---
+++ instamining.py
@@ -0,0 +1,31 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+
+main_hashtag = "dog"
+
+browser.get(f"https://www.instagram.com/explore/tags/{main_hashtag}")
+
+header = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.TAG_NAME, "header"))
+)
+
+hashtags = header.find_elements_by_class_name("AC7dP")
+
+for hashtag in hashtags:
+    ActionChains(browser).key_down(Keys.CONTROL).click(hashtag).perform()
+
+for window in browser.window_handles:
+    browser.switch_to.window(window)
+    hashtag_name = browser.find_element_by_tag_name("h1")
+    print(hashtag_name.text[1:])
+
+time.sleep(3)
+browser.quit()
